0929d2ed4f33187c2bb875fe65f38b3409bcf8bd
ual4720 <31104211+ual4720@users.noreply.github.com>
2019-07-19 14:35:10 -0700

    Header Alignment Fixes

--- application/views/templates/header_small.php
+++ application/views/templates/header_small.php
@@ -1,9 +1,9 @@
 <!-- Header -->
-<header class="w3-container w3-center w3-theme w3-padding w3-site" id="siteHeader">
-	<div class="w3-section">
-		<div id="w3-site-navigation"><i onclick="w3_open()" class="fa fa-bars w3-xlarge w3-button w3-theme w3-left"></i></div>
-		<div id="w3-site-name"><h3><?php echo $organization->name; ?> - <?php echo $organization->chapter; ?></h3></div>
-		<div id="w3-site-logo" style="float:right;"><img src="<?php echo $images . "brand/ACTS-Int-Logo-large-.png"; ?>" title="<?php echo $organization->name; ?> Logo" class="w3-right" height="45px" /></div>
+<header class="w3-container w3-theme w3-padding w3-site" id="siteHeader">
+	<div class="w3-half">
+		<div id="w3-site-navigation" class="w3-left"><h3><i onclick="w3_open()" class="fa fa-bars w3-button"></i></h3></div>
+		<div id="w3-site-name" class="w3-left w3-padding-small"><h3><?php echo $organization->name; ?> - <?php echo $organization->chapter; ?></h3></div>
+		<!--<div id="w3-site-logo" class="w3-left"><img src="<?php //echo $images . "brand/ACTS-Int-Logo-large-.png"; ?>" title="<?php //echo $organization->name; ?> Logo" class="w3-right" height="45px" /></div>-->
 	</div>
 </header>
 <div class="w3-container w3-padding-0">
